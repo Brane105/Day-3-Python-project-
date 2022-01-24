48d78acdddb37fa516359a8c97d8d16287f51cd4
Bhalchandra Rane <93726807+Brane105@users.noreply.github.com>
2022-01-24 04:37:17 -0800

    Add files via upload

---
+++ pizza order.py
@@ -0,0 +1,32 @@
+from audioop import add
+
+
+print("Welcome to Python Pizza Deliveries!")
+size = input("What size pizza do you want? S, M, or L ").lower()
+add_pepperoni = input("Do you want pepperoni? Y or N ").lower()
+extra_cheese = input("Do you want extra cheese? Y or N ").lower()
+# code to pizza order 
+bill = 0 
+if size == 's':
+    bill += 15
+    if add_pepperoni == 'y':
+            bill += 2
+    elif extra_cheese == 'y':
+                    bill += 1
+elif size == 'm':
+    bill += 20
+    if add_pepperoni == 'y':
+            bill += 3
+    elif extra_cheese == 'y':
+                    bill += 1
+elif size == 'l':
+    bill +=25
+    if add_pepperoni == 'y':
+            bill += 3
+    elif extra_cheese == 'y':
+                    bill += 1
+else : 
+    print("Invalid input 'plz try again !'")
+
+print(f'Your total amount is ${bill}')
+
